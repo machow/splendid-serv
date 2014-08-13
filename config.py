ecce9f8bea8ec6698e86be317e5978be53d76ba9
Michael Chow <machow@princeton.edu>
2014-08-13 05:04:27 -0400

    added heroku secret keys

--- config.py
+++ config.py
@@ -19,11 +19,14 @@ from authomatic.providers import oauth2, oauth1, openid
 import authomatic
 SCOPES = ['https://www.googleapis.com/auth/userinfo.email',
           'https://www.googleapis.com/auth/userinfo.profile']
+
+hk = os.environ.get('HEROKU') and '538317491200-tksa2trrvuaav994drjmsj7ndbkvtvqm.apps.googleusercontent.com'
+hs = os.environ.get('HEROKU') and 'nPUDPgJMjY8J1CD6mYIIydsY'
 LOGINS = {
         'google': {
             'class_': oauth2.Google,
-            'consumer_key': '538317491200-tfrhfi6u5k7mi2uil4afkm1j83mou7so.apps.googleusercontent.com',
-            'consumer_secret': '407DcNKLLCLOk3hof4NxLgGG',
+            'consumer_key': hk or '538317491200-tfrhfi6u5k7mi2uil4afkm1j83mou7so.apps.googleusercontent.com',
+            'consumer_secret': hs or '407DcNKLLCLOk3hof4NxLgGG',
             'id': authomatic.provider_id(),
             'scope': SCOPES#oauth2.Google.user_info_scope
         }
