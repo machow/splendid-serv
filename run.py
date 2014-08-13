fcc76f13d34991b74aaa0395341e2da9a48895ff
Michael Chow <machow@princeton.edu>
2014-08-13 02:02:11 -0400

    changed to cast port to int i hate my life

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/Users/machow/venv/splendid/bin/python
 import os
 from app import app
-port = os.environ.get('$PORT') or 5000
-app.run(threaded=False, port=port)
+
+app.run(threaded=False, port=int(os.environ.get('$PORT', 5000)))
