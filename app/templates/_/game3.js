fe6a2b6d2e09681ee9c489911f5c7df2a38d544b
Michael Chow <machow@princeton.edu>
2014-11-13 21:13:35 -0500

    First working angular steps for board
    controller is in app/templates/_game3.js
    views are in app/templates/_/views
    main page is app/templates/game3.html

---
+++ app/templates/_/game3.js
@@ -0,0 +1,9 @@
+(function(){
+    var app = angular.module('splendid', []);
+
+    app.controller('boardController', ['$http', function ($http) {
+        board = this;
+        board.state = gamedat;
+    }]);
+
+})();
